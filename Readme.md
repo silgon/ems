cec6f9c317e426daa586cb55893a9e6f56082a5a
silgon <silgon3200@gmail.com>
2024-08-23 14:37:32 +0200

    add|script: starter script

--- Readme.md
+++ Readme.md
@@ -1,6 +1,13 @@
 # Emoji Search Bash (EMS)
 This is a small script I decided to make because I needed some emojis (also because I spend most of my time in the shell).
 
+## Installation
+
+```bash
+bash <(curl -s https://github.com/silgon/ems/raw/main/starter.sh)
+```
+
+The dependencies are `jq` and `xclip`. The script will try to install them if possible.
 
 
 This is inspired on [wofi-emoji](https://github.com/Zeioth/wofi-emoji). Actually, it was me trying to use it and I realized i needed wayland (which I do not want). I read the code and I created this small repo.
---
+++ starter.sh
@@ -0,0 +1,51 @@
+# Function to install a package using the appropriate package manager
+install_package() {
+    PACKAGE=$1
+    if command_exists apt-get; then
+        sudo apt-get update && sudo apt-get install -y "$PACKAGE"
+    elif command_exists dnf; then
+        sudo dnf install -y "$PACKAGE"
+    elif command_exists yum; then
+        sudo yum install -y "$PACKAGE"
+    elif command_exists pacman; then
+        sudo pacman -Sy --noconfirm "$PACKAGE"
+    else
+        echo "Error: Package manager not found. Please install $PACKAGE manually."
+        exit 1
+    fi
+}
+
+# Check for required dependencies: jq and xclip
+if ! command_exists jq; then
+    echo "'jq' is not installed. Installing..."
+    install_package jq
+fi
+
+if ! command_exists xclip; then
+    echo "'xclip' is not installed. Installing..."
+    install_package xclip
+fi
+
+# Download the file
+URL="https://github.com/silgon/ems/raw/main/ems"
+FILENAME=ems
+
+curl -L -o "$FILENAME" "$URL"
+if [ $? -ne 0 ]; then
+    echo "Error downloading file. Please check the URL and try again."
+    exit 1
+fi
+
+# Make the file executable
+chmod +x "$FILENAME"
+
+# Move the file to ~/bin (create it if it doesn't exist)
+mkdir -p ~/bin
+mv "$FILENAME" ~/bin/
+
+# Add ~/bin to PATH if it's not already in PATH
+if [[ ":$PATH:" != *":$HOME/bin:"* ]]; then
+    echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
+    export PATH="$HOME/bin:$PATH"
+fi
+
